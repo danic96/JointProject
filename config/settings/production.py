3c7d6fdb612523f5e1991b5980d61df3dce44879
danic96 <you@example.com>
2019-08-08 11:30:20 +0000

    multi-enviroment running

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,1 +1,12 @@
 from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jointproject',
+        'USER': 'user',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
